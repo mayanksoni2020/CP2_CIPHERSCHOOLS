7c8197189fed1511588de32e3e1612140b0c8b3f
Mayank Soni <48762957+mayanksoni2020@users.noreply.github.com>
2020-08-20 10:23:43 +0000

    Assignment 2:- Merge two sorted list

---
+++ MergeTwoSortedList.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if(!l1)return l2;
+        if(!l2)return l1;
+        ListNode* res=NULL;
+        if(l1->val<=l2->val){
+            res=l1;
+            l1=l1->next;
+        }
+        else{
+            res=l2;
+            l2=l2->next;
+        }
+        ListNode* temp=res;
+        while(l1 and l2){
+            if(l1->val<=l2->val){
+                temp->next=l1;
+                l1=l1->next;
+            }
+            else{
+                temp->next=l2;
+                l2=l2->next;
+            }
+            temp=temp->next;
+        }
+        if(l1)temp->next=l1;
+        if(l2)temp->next=l2;
+        return res;
+    }
+};
